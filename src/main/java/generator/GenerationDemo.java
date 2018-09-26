c3cc30ffc49c054b3ea68e4bf4cb34262731fd6b
sk0rn <hyperthrone@gmail.com>
2018-09-26 23:10:45 +1100

    update Demo and .gitignore

--- src/main/java/generator/GenerationDemo.java
+++ src/main/java/generator/GenerationDemo.java
@@ -6,13 +6,13 @@ public class GenerationDemo {
 
     public static void main(String[] args) {
 
-        String path = "D:\\dev_edu\\STC13_HT\\HT04_TrashGenerator\\src\\data\\";
+        String path = "D:\\dev_edu\\STC13_HT\\HT04_TrashGenerator\\src\\main\\java\\data\\";
 
         DataGen generator = new DataGen();
         String[] randomWords = generator.genArrayWords(20);
 
         long start = System.currentTimeMillis();
-        generator.getFiles(path, 10,200, 300, 1, randomWords, 10);
+        generator.getFiles(path, 5,200, 300, 1, randomWords, 10);
         System.out.println(System.currentTimeMillis() - start);
     }
 }
